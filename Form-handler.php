0c295448e5b02c39b0852109c112c66fc650202d
Isaacxavierlee <101383340+Isaacxavierlee@users.noreply.github.com>
2024-02-17 01:16:57 +0000

    c

--- Form-handler.php
+++ Form-handler.php
@@ -1,26 +1,30 @@
 <?php
-$name = $_POST['name'];
-$visitor_email = $_POST['email'];
-$subject = $_POST['subject'];
-$message = $_POST['message'];
+if(isset($_POST['submit'])) {
+  $name = $_POST['name'];
+  $visitor_email = $_POST['email'];
+  $subject = $_POST['subject'];
+  $message = $_POST['message'];
 
-$email_from = 'info@https://ixlunipage.netlify.app';
+  $email_from = 'info@ixlunipage.netlify.app'; // Update with your email address
 
-$email_subject = "New Form submission";
+  $email_subject = "New Form submission";
 
-$email_body = "User Name: $name.\n".
-              "User Email: $visitor_email.\n".
-              "Subject: $subject.\n".
-              "User Message: $message.\n";
+  $email_body = "User Name: $name.\n".
+                "User Email: $visitor_email.\n".
+                "Subject: $subject.\n".
+                "User Message: $message.\n";
 
-$to = 'reivax.caasi@gmail.com';
+  $to = 'reivax.caasi@gmail.com'; // Update with your email address
 
-$headers = "From: $email_from \r\n";
+  $headers = "From: $email_from \r\n";
+  $headers .= "Reply-To: $visitor_email \r\n";
 
-$headers .= "Reply-To: $visitor_email \r\n";
+  mail($to, $email_subject, $email_body, $headers);
 
-mail($to,$email_subject,$email_body,$headers);
-
-header("Location: contact.html");
-
+  // Redirect to a thank you page after sending the email
+  header("Location: thank-you.html");
+} else {
+  // If the form is not submitted, redirect back to the contact page
+  header("Location: contact.html");
+}
 ?>
